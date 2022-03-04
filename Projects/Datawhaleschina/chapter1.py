61bdf84ce64e104f5ca0125540681c103573edc7
zzhang <zzhang@mhfunds.com>
2022-03-04 16:13:14 +0800

    add policy iteration method

--- Projects/Datawhaleschina/chapter1.py
+++ Projects/Datawhaleschina/chapter1.py
@@ -8,10 +8,12 @@ for _ in range(10):
     print(observation)
 env.close()
 
+
 from gym import envs
 env_specs = envs.registry.all()
 envs_ids = [env_spec.id for env_spec in env_specs]
 print(envs_ids)
+
 
 import gym
 env = gym.make('MountainCar-v0')
@@ -20,4 +22,8 @@ print('动作空间 = {}'.format(env.action_space))
 print('观测范围 = {} ~ {}'.format(env.observation_space.low,
         env.observation_space.high))
 print('动作数 = {}'.format(env.action_space.n))
+
+
+if __name__ == '__main__':
+    pass
 
---
+++ Projects/Datawhaleschina/chapter2.py
@@ -0,0 +1,133 @@
+# Chapter 2
+# Mainly focus on MDP 
+# Value function
+# Q function
+# Value iteration
+# Policy iteration
+"""
+Realize policy iteration code
+Realize value iteration code
+Reference:
+https://github.com/cuhkrlcourse/RLexample/blob/master/MDP/frozenlake_policy_iteration.py
+https://github.com/cuhkrlcourse/RLexample/blob/master/MDP/frozenlake_value_iteration.py
+"""
+
+"""
+Policy iteration Algorithms:
+
+"""
+
+import numpy as np
+import gym
+from gym import wrappers
+from gym.envs.registration import register
+
+def frozen_lake_policy_iteration():
+
+    def compute_policy_v(policy, gamma):
+        """Compute the value function for given policy
+
+        Args:
+            policy (np.ndarray): given a policy
+            gamma (float): discount factor
+
+        Returns:
+            np.ndarray: value function
+
+        Notice:
+        env.env.P is a dict: observation_space -> list[(possible, next_sate, reward, info)]
+        which means, if you want to access to next 
+        """
+        eps = 1e-10
+        v = np.zeros(env.observation_space.n)
+        while True: # iteratively compute the value function until convergence
+            prev_v = np.copy(v)
+            for s in range(env.observation_space.n):
+                a = policy[s]
+                v[s] = sum([p * (r + gamma * prev_v[s_]) for p, s_, r, _ in env.env.P[s][a]])
+            if np.sum((np.fabs(prev_v - v))) <= eps:
+                break
+        return v
+    
+    def extract_policy(v, gamma):
+        """Calculate an optimal policy given a value function
+
+        Args:
+            v (np.ndarray): value function
+            gamma (_type_): discount factor
+
+        Returns:
+            np.ndarray: optimal policy
+        """
+        policy = np.zeros(env.observation_space.n)
+        for s in range(env.observation_space.n):
+            q_sa = np.zeros(env.action_space.n)
+            for a in range(env.action_space.n):
+                q_sa[a] = sum([p * (r + gamma * v[s_]) for p, s_, r, _ in env.env.P[s][a]])
+            policy[s] = np.argmax(q_sa)
+        return policy
+
+    def evaluate_policy(policy, gamma, n):
+        """Evaluate how a policy performes
+
+        Args:
+            policy (np.ndarray): policy
+            gamma (float): discount factor
+            n (int): iteration times
+
+        Returns:
+            float: scores
+        """
+        scores = [run_episode(policy=policy, gamma=gamma, render=False) for _ in range(n)]
+        return np.mean(scores)
+
+    def run_episode(policy, gamma, render=False):
+        """Run an episode and return a total reward given a policy
+
+        Args:
+            policy (np.ndarray): policy
+            gamma (float): discount factor
+            rendor (bool): whether rendor or not
+
+        Returns:
+            float: total_reward
+        """
+        obs = env.reset()
+        total_reward = 0.0
+        step_idx = 0
+        while True:
+            if render:
+                env.render()
+            obs, reward, done, _ = env.step(int(policy[obs]))
+            total_reward += (gamma ** step_idx * reward)
+            step_idx += 1
+            if done:
+                break
+        return total_reward
+    
+    # create frozen lake environment 
+    env_name = 'FrozenLake-v1'
+    env = gym.make(env_name)
+
+    # Get Optimal Policy
+    policy = np.random.choice(env.action_space.n, size=(env.observation_space.n)) # initial a random policy
+    max_iteration = 1000000
+    gamma = 0.99
+    n = 100
+    for i in range(max_iteration):
+        old_value_v = compute_policy_v(policy=policy, gamma=gamma)
+        new_policy = extract_policy(v=old_value_v, gamma=gamma)
+        if (np.all(policy == new_policy)):
+            print("Policy-Iteration converged at step %d." %(i+1))
+            break
+        policy = new_policy
+    scores = evaluate_policy(policy, gamma, n)
+    print("Average scores = ", np.mean(scores))
+    return 
+
+
+def frozen_lake_value_iteration():
+    pass
+
+if __name__ == '__main__':
+    frozen_lake_policy_iteration()
